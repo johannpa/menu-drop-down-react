777e3ffb3856dd378aa61ee38ce2e6f5359ff6ad
johannpa <johann_paque@hotmail.fr>
2022-04-24 14:30:22 +0200

    menu drop down

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        value: 'coconut', 
+        countries: ["Cambodia", "Austrilia", "US"]
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+//   handleChange(event) {
+//     this.setState({value: event.target.value});
+//   }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  handleChange = (e) => {
+      this.setState({value: e.target.value});
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <br />
+        <br />
+        <label>
+            Looping through Array
+            <select>
+                {this.state.countries.map(country => (
+                    <option key={country} value={country}>
+                        {country}
+                    </option>
+                ))}
+            </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector("#root"));
